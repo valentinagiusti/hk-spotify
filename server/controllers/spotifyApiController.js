71bf458fdc90c833244ed55ff92e290465312422
Valentina Giusti <valentinamgiusti@gmail.com>
2022-05-11 16:51:28 -0300

    Update .env.example

--- server/controllers/spotifyApiController.js
+++ server/controllers/spotifyApiController.js
@@ -1,6 +1,4 @@
 const axios = require("axios");
-require("dotenv").config();
-const _ = require("lodash");
 const apiURL = process.env.SPOTIFY_API_URL;
 
 const { PrismaClient } = require("@prisma/client");
@@ -9,14 +7,18 @@ const prisma = new PrismaClient();
 const { getApiToken } = require("./getApiToken");
 const token = getApiToken();
 
+const _ = require("lodash");
+
 async function searchForArtist(req, res) {
   try {
+    // register search on database
     await prisma.request.create({
       data: {
         artist: req.body.artist,
         userip: req.ip,
       },
     });
+    //search the first artists on the list
     const searchArtist = await axios.get(
       `${apiURL}/search?query=${req.body.artist}&type=artist&market=us&limit=1`,
       {
@@ -26,7 +28,7 @@ async function searchForArtist(req, res) {
         },
       },
     );
-
+    //search albums of the artist with id obtained from previous step
     const artistId = searchArtist.data.artists.items[0].id;
 
     const includeAlbums = await axios.get(`${apiURL}/artists/${artistId}/albums?market=UY`, {
@@ -37,7 +39,7 @@ async function searchForArtist(req, res) {
     });
 
     const albumIds = includeAlbums.data.items.map((album) => album.id);
-
+    //with all ids search for the albums
     const response = await axios.get(`https://api.spotify.com/v1/albums`, {
       params: { ids: albumIds.join(",") },
       headers: {
@@ -45,7 +47,7 @@ async function searchForArtist(req, res) {
         "Content-Type": "application/x-www-form-urlencoded",
       },
     });
-
+    //order albums by popularity with external library lodash
     res.json({
       albums: _.orderBy(response.data.albums, ["popularity"], ["desc"]),
       artist: searchArtist.data.artists,
@@ -54,7 +56,7 @@ async function searchForArtist(req, res) {
     console.error(error);
   }
 }
-
+//get all searches from database
 async function getAll(req, res) {
   try {
     const allRequests = await prisma.request.findMany();
